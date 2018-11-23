47ef428230dc6d017ae997af7ade0158921d6074
Lokasenna <jalovatt@hotmail.com>
2018-11-22 20:07:35 -0700

    Update content and styling

--- scripts/app.js
+++ scripts/app.js
@@ -2,9 +2,18 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const html = document.querySelector("html");
   const toggle = document.getElementById("toggle");
+
   toggle.addEventListener("click", (event) => {
+
     event.preventDefault();
-    html.classList.toggle("dark-mode");
+
+    const mode = event.target.value;
+    html.classList.toggle("theme-one-light", mode === "one-light")
+    html.classList.toggle("theme-one-dark", mode === "one-dark")
+    html.classList.toggle("theme-monokai", mode === "monokai")
+
+    toggle.style.width = String(`[THEME: ${mode}]`).length + "ch";
+
   });
 
 });
