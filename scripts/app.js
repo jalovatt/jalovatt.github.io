ba846ac9062f6339c18602732a4c35d4bb56a54b
Lokasenna <jalovatt@hotmail.com>
2018-12-01 20:20:38 -0700

    Continue refactor, add light Monokai theme

--- scripts/app.js
+++ scripts/app.js
@@ -10,7 +10,8 @@ document.addEventListener("DOMContentLoaded", () => {
     const mode = event.target.value;
     html.classList.toggle("theme-one-light", mode === "one-light");
     html.classList.toggle("theme-one-dark", mode === "one-dark");
-    html.classList.toggle("theme-monokai", mode === "monokai");
+    html.classList.toggle("theme-monokai-light", mode === "monokai-light");
+    html.classList.toggle("theme-monokai-dark", mode === "monokai-dark");
 
     toggle.style.width = String(`[THEME: ${mode}]`).length + "ch";
 
