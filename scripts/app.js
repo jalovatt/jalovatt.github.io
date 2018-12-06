946fbccc84bca130d8ba6e72d396affe0686919d
Adam Lovatt <jalovatt@hotmail.com>
2018-12-06 16:06:24 -0700

    Tweak styling

--- scripts/app.js
+++ scripts/app.js
@@ -6,12 +6,9 @@ document.addEventListener("DOMContentLoaded", () => {
   toggle.addEventListener("change", (event) => {
 
     event.preventDefault();
-
     const mode = event.target.value;
-    html.classList.toggle("theme-one-light", mode === "one-light");
-    html.classList.toggle("theme-one-dark", mode === "one-dark");
-    html.classList.toggle("theme-monokai-light", mode === "monokai-light");
-    html.classList.toggle("theme-monokai-dark", mode === "monokai-dark");
+
+    html.className = `theme-${mode}`;
 
     toggle.style.width = String(`[THEME: ${mode}]`).length + "ch";
 
